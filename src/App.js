6ed0351477140f92996222f98fe38f63036bf5a3
SamRCrespo <samcre8997@students.ecpi.edu>
2024-08-23 17:39:52 -0400

    deploy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           Samuel Crespo React App is deployed by using Vercel 
+           Samuel Crespo React App is deployed by using Netlify 
         </p>
       </header>
     </div>
